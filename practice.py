64eae6895144ec2012873bd6191dc4d760c11405
Matheka Lawrence <90851966+mathekalawrence@users.noreply.github.com>
2025-08-28 14:00:06 +0300

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,12 @@
+
+"""
+if angered::
+    print("I want to rest early!")
+"""
+temperature = 60
+if temperature<40:
+    print("The heat is too much!!")
+elif temperature>45:
+    print("The temperature is moderate")
+else:
+    print("it is cooler")
