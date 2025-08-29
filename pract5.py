8a63828acb3b789f861c23d89f9e20274a70ca1d
Matheka Lawrence <90851966+mathekalawrence@users.noreply.github.com>
2025-08-29 09:39:28 +0300

    Add files via upload

--- pract5.py
+++ pract5.py
@@ -1,5 +1,6 @@
 #OOP
 #Defining a class
+"""
 class Car:
     color = "green"
 
@@ -11,3 +12,17 @@ class Car:
 my_car = Car()
 print(my_car.color)
 my_car.drive()
+"""
+
+class Car:
+    def _init_(self, color, model):
+        self.color = color #Instance variable
+        self.model = model #Instance variable
+        
+#Creating objects with unique attributes
+car1 = Car("blue",  "Tesla")
+car2 = Car("yellow", "Honda")
+
+#Output
+print(car1.color) 
+print(car2.model)
